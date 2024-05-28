d208e6878567f0f1a56e5684beae1a90877b372c
Dagale <144978155+Raia31@users.noreply.github.com>
2024-05-29 06:57:44 +0800

    initial commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDhqOQlvhm2c2I4uUdJsiUS65m68hX_rtw",
+  authDomain: "crud-d3f76.firebaseapp.com",
+  databaseURL: "https://crud-d3f76-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-d3f76",
+  storageBucket: "crud-d3f76.appspot.com",
+  messagingSenderId: "562236609365",
+  appId: "1:562236609365:web:b3a9a8537c7f8db33515dc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ myscript.js
@@ -0,0 +1,91 @@
+var rollV, nameV, genderV, addressV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  bloodtype1 = document.getElementById("bloodtype").value;
+  emailaddress2 = document.getElementById("emailaddress").value;
+  Swal.fire("Succesfully Read Data!");
+  console.log(rollV, nameV, addressV, genderV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      bloodtype: bloodtype1,
+      emailaddress: emailaddress2,
+
+      
+    });
+    Swal.fire("Succesfully Insert Data!");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("bloodtype").value = "";
+  document.getElementById("emailaddress").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
+      document.getElementById("bloodtype").value = snap.val().bloodtype;
+      document.getElementById("emailaddress").value = snap.val().emailaddress;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      bloodtype: bloodtype1,
+      emailaddress: emailaddress2,
+    });
+    Swal.fire("Succesfully Update Data!");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("bloodtype").value = "";
+  document.getElementById("emailaddress").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+    Swal.fire("Succesfully Delete Data!");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("bloodtype").value = "";
+  document.getElementById("emailaddress").value = "";
+};
